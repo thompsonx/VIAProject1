5c7a0266a880f27ce9dec1f37989045a3fd78d35
Tomáš Panoc <8thompson8@seznam.cz>
2015-12-01 18:27:35 +0100

    Finished

--- VIAProject1/js/app.js
+++ VIAProject1/js/app.js
@@ -5,11 +5,11 @@ var msg_api_error = "Incorrect call of the forecast API! Check city you have ent
 function displayData(data) {
     var days = Number($("#wf-days").val());
 
-    $("#chart")[0].remove();
+    $("#chart").remove();
     var canvas = document.createElement("canvas");
     canvas.id = "chart";
-    canvas.setAttribute("width", "900");
-    canvas.setAttribute("height", "500");
+    canvas.width = 800;
+    canvas.height = 400;
     $("#main").append(canvas);
     canvas = canvas.getContext("2d");
 
@@ -57,37 +57,16 @@ function displayData(data) {
     }
 
     var graphoptions = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
         scaleBeginAtZero : atzero,
-
-        //Boolean - Whether grid lines are shown across the chart
         scaleShowGridLines : true,
-
-        //String - Colour of the grid lines
         scaleGridLineColor : "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
         scaleGridLineWidth : 1,
-
-        //Boolean - Whether to show horizontal lines (except X axis)
         scaleShowHorizontalLines: true,
-
-        //Boolean - Whether to show vertical lines (except Y axis)
         scaleShowVerticalLines: true,
-
-        //Boolean - If there is a stroke on each bar
         barShowStroke : true,
-
-        //Number - Pixel width of the bar stroke
         barStrokeWidth : 2,
-
-        //Number - Spacing between each of the X value sets
         barValueSpacing : 5,
-
-        //Number - Spacing between data sets within X values
         barDatasetSpacing : 2,
-
-        //String - A legend template
         legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 
     };
@@ -105,6 +84,10 @@ function displayError(message, element) {
 $("form").submit(
     function (event) {
         event.preventDefault();
+
+        $(".error-message").remove();
+        $(".empty-graph").remove();
+
         var city = $("#wf-city").val();
 
         var re = /^[\w]*$/;
